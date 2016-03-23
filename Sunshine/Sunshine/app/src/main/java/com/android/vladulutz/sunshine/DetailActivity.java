2fcb30e5790dd37087e94a352e089842f862077f
Vlad Matei <vladmatei139@gmail.com>
2016-03-23 10:12:33 +0200

    4a finished

--- Sunshine/Sunshine/app/src/main/java/com/android/vladulutz/sunshine/DetailActivity.java
+++ Sunshine/Sunshine/app/src/main/java/com/android/vladulutz/sunshine/DetailActivity.java
@@ -2,13 +2,10 @@ package com.android.vladulutz.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -19,14 +16,6 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
